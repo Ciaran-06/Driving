e2de11d95d45312a3220f80c47a2b59147056a1e
Ciaran-06 <76187734+Ciaran-06@users.noreply.github.com>
2022-02-01 18:32:19 +0000

    made testing branch

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
 
+const loader = new THREE.TextureLoader();
+var bgTexture = loader.load("./textures/background.jpg");
+
+scene.background = bgTexture;
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -9,6 +14,13 @@ const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+const plane = new THREE.PlaneGeometry(10, 10);
+const pMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+const pGeometry = new THREE.Mesh(plane, pMaterial);
+pGeometry.rotation.x = -Math.PI / 2;
+pGeometry.position.y = -0.5;
+scene.add(pGeometry);
 
 camera.position.z = 4;
 camera.position.y = 0;
@@ -19,8 +31,6 @@ function animate() {
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-
-
 
     render();
 };
@@ -37,5 +47,4 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight)
     render()
 }
-
 animate();
